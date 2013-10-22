14f86011f6a408732f4ed0c312d64b962c63246d
Inge Seim <inge.seim@gmail.com>
2013-10-22 11:37:41 +1000

    Update mergeorama.sh
    more embarrassing bugfixes

--- v2.0/mergeorama.sh
+++ v2.0/mergeorama.sh
@@ -3,46 +3,64 @@
 ################################################################################################################################################################
 # mergeorama.sh - this script does something awful.
 # By Inge Seim
-# 10/2013 v2
+# 10/2013 v3
 #
 #  this script parses files and batches a dodgy Perl script
 # 
 ################################################################################################################################################################
 
-# Dummy run. This way you can add whatever dummy X axis you want
-perl mergefiles.pl pixelID.txt pixelID.txt ;
-cp largerfile.txt ./data ;
-rm ./largerfile.txt ; # get rid of your temp files
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+
+
+# fix file name's first!
 #convert data files spaces to _/underscore if you have weird Windows file names etc.
-cd ./data ;  #~
+cd ./data ;  
 ls *.asc | while read -r FILE  
 do
     mv -v "$FILE" `echo $FILE | tr ' ' '_' `
 done
+
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
 # convert all *.asc (data files) to files where 0 is replaced with a dash/-)
 for i in *.asc ; do
 perl -pi -e 's/\s0\s/\ \-\n/g' $i ;
-# perl ../zeroToDeci.pl $i ;
-#@ cp $i $i.bac ;
-#@ rm $i ;
-#@ cp $i $i.input ;  # your input data files no longer contains just zeros! 
 done
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# dummy run. Use the x-values from the first file in the directory as a guide
+FILE=$(ls | sort -n | head -1)
+cp $FILE ../input.txt ; 
+# \rm `ls | sort -n | head -1` 
+cd ../ ;
+# add header
+perl -pi -le 'print "PixelID dummy" if $. == 1' input.txt ;   #  ; # Add this line to the top\n";
+# Dummy run
+perl mergefiles.pl input.txt input.txt ;
+cp largerfile.txt ./data ;
+rm ./temp.txt ; # get rid of your temp files
+rm ./largerfile.txt ; # get rid of your temp files
+cd ./data ;
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# run the script on all your datafiles 
 for j in *.asc ; do
 perl ../mergefiles.pl largerfile.txt $j ;
 rm ./temp.txt ; # get rid of your temp files
-rm ../temp.txt ; # get rid of your temp files
 done
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
 # get rid of the dummy columns
-cut -f1,4- ./largerfile.txt >../output.csv ;  # column 2 and 3 contain "dummy" 
+cut -f1,4- ./largerfile.txt >../output.csv ;  # column 1 and 2 contain "dummy" 
 rm ./largerfile.txt ; # get rid of your temp files
+rm ../input.txt ;
 # use the unix command cut for this
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
 # convert dashes back to zero
 perl -pi -e 's/-/0/g' ../output.csv ;
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
 # unnecessary echo here 
 clear ;
@@ -55,3 +73,4 @@ echo '
    O  || |
    |   D J
   // /"""'
+
